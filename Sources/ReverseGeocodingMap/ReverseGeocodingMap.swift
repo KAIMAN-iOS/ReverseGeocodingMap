434b8c902313fd714508c725d8bac0a099df7e2f
GG <jerometonnelier@yahoo.fr>
2020-10-22 16:32:39 +0200

    first commit

--- Sources/ReverseGeocodingMap/ReverseGeocodingMap.swift
+++ Sources/ReverseGeocodingMap/ReverseGeocodingMap.swift
@@ -1,3 +1,18 @@
-struct ReverseGeocodingMap {
-    var text = "Hello, World!"
+import MapKit
+import UIKit
+
+class ReverseGeocodingMap: UIViewController {
+    @IBOutlet weak var map: MKMapView!  {
+        didSet {
+            map.delegate = self
+        }
+    }
+
+    @IBOutlet weak var centerImage: UIImageView!
+}
+
+extension ReverseGeocodingMap: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        
+    }
 }
